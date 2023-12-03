d3774f923c37c0cd19ed78c6360c92075cf1caa3
Joshua Daley <jddaley328@gmail.com>
2023-12-03 16:16:19 -0600

    added clown table

--- backend/db_tables.php
+++ backend/db_tables.php
@@ -24,9 +24,9 @@ if ($conn->connect_error) {
 $sql = "CREATE TABLE AnimalPlushie (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Itemname VARCHAR(30) NOT NULL,
-    itemnumber VARCHAR(30) NOT NULL,
-    stock VARCHAR(50),
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    price VARCHAR(30) NOT NULL,
+    stock VARCHAR(50)
+    
     )";
     
     if (mysqli_query($conn, $sql)) {
@@ -35,6 +35,23 @@ $sql = "CREATE TABLE AnimalPlushie (
       echo "Error creating table: " . mysqli_error($conn);
     }
 
+
+
+
+    $sql = "CREATE TABLE ClownPlushie (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        Itemname VARCHAR(30) NOT NULL,
+        price VARCHAR(30) NOT NULL,
+        stock VARCHAR(50)
+       
+        )";
+        
+        if (mysqli_query($conn, $sql)) {
+          echo "Table Clownplusie created successfully";
+        } else {
+          echo "Error creating table: " . mysqli_error($conn);
+        }
+    
 
     $conn->close(); 
 ?> 
