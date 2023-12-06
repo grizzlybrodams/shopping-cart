eed7b6144c6efc094746ecbb89a280f0e5fe9583
Joshua Daley <jddaley328@gmail.com>
2023-12-05 20:27:34 -0600

    add js and some icons

---
+++ backend/db.php
@@ -0,0 +1,15 @@
+<?php
+function OpenCon()
+{
+$dbhost = "localhost";
+$dbuser = "root";
+$db = "example";
+
+$conn = new mysqli($dbhost, $dbuser,$dbname) or die("Connect failed: %s\n". $conn -> error);
+return $conn;
+}
+function CloseCon($conn)
+{
+$conn -> close();
+}
+?>
