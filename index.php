b7c14ca1f13a7765498d659f17dcb639bf831e43
Joshua Daley <jddaley328@gmail.com>
2023-12-12 12:21:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -150,7 +150,7 @@ if(!empty($_SESSION["shopping_cart"])) {
 $cart_count = count(array_keys($_SESSION["shopping_cart"]));
 ?>
 <div class="cart_div">
-<a href="cart.php"><img src="cart-icon.png" width="50px" height="50px" /> Cart<span>
+<a href="cart.php"><img src="cart-icon.png" width="50px" height="50px" class="cart-img" /> Cart<span>
 <?php echo $cart_count; ?></span></a>
 </div>
 <?php
